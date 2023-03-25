2250843e846f9e088bf1036a78af62aaedada253
siddhant-afk <siddhantdudhane21@gmail.com>
2023-03-25 11:38:53 +0530

    Create Preference table for database

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+
+class Preference(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    topics = db.Column(db.String())
+    location = db.Column(db.String())
 
 @app.route("/")
 def home():
