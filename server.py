7bbbe9ed24c9decace9105401565b8987001cd58
siddhant-afk <siddhantdudhane21@gmail.com>
2023-04-14 15:52:49 +0530

    Create User table

--- server.py
+++ server.py
@@ -6,10 +6,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
 
-class Preference(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
+    email_address = db.Column(db.String(length=50),nullable=False, unique=True)
+    password = db.Column(db.String(length=60),nullable = False)
     topics = db.Column(db.String())
     location = db.Column(db.String())
+
+
+    def __repr__(self):
+        return self.email_address
+    
+
+
+
+
+    
 
 
 @app.route("/login")
@@ -24,4 +36,5 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
